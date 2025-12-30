0ea9a215d9d9ff7c89b39ee538c4b9b5ab89a8cf
Eduard Drusa <ventyl86@netkosice.sk>
2025-12-30 15:54:24 +0100

    Feat: Add button gradient and sending of event
    Button now has background gradient, border reacts to button depression
    and once mouse button is up, the button sends event if it was configured
    to do so.

--- src/contraption/gadgets/button.c
+++ src/contraption/gadgets/button.c
@@ -2,13 +2,28 @@
 #include <render.h>
 #include "button.h"
 #include <resources.h>
+#include <client.h>
+#include <gadget.h>
 
+#define COLOR_HIGHLIGHT 0xE3E3E3FF
+#define COLOR_SHADE 0x9FABBCFF
 
 void render_button(const struct CWindowInternal * window, const struct CGadgetInternal * gadget)
 {
-    struct CExtent extents;
+    struct CExtent extents, text_extents;
     contraption_calc_extent(window, gadget, &extents);
-    contraption_render_frame(&extents, BUTTON_RADIUS, 0xE3E3E3FF, 0x9FABBCFF);
+    text_extents = extents;
+    uint32_t hi_color = COLOR_HIGHLIGHT, lo_color = COLOR_SHADE;
+    if (gadget->properties.state & GADGET_STATE_ACTIVATED)
+    {
+        hi_color = COLOR_SHADE;
+        lo_color = COLOR_HIGHLIGHT;
+        // Simulate pressing the button by moving text one px right and down
+        text_extents.top += 2;
+        text_extents.left += 2;
+    }
+    contraption_render_background(&extents, &button_background, button_pixmap);
+    contraption_render_frame(&extents, BUTTON_RADIUS, hi_color, lo_color);
     contraption_render_text(&extents, gadget->properties.text, gadget->properties.text_flags, gadget->properties.fg_color, gadget->properties.text_margin_horiz, gadget->properties.text_margin_vert);
 }
 
@@ -19,12 +34,27 @@ void pointer_button(struct CWindowInternal * window, struct CGadgetInternal * ga
 
 void button_down_button(struct CWindowInternal * window, struct CGadgetInternal * gadget, uint32_t buttons)
 {
-
+    if (gadget->properties.flags & GADGET_FLAG_ENABLED)
+    {
+        if ((gadget->properties.state & GADGET_STATE_ACTIVATED) == 0)
+        {
+            gadget->properties.state |= GADGET_STATE_ACTIVATED;
+            render_button(window, gadget);
+        }
+    }
 }
 
 void button_up_button(struct CWindowInternal * window, struct CGadgetInternal * gadget, uint32_t buttons)
 {
-
+    if (gadget->properties.flags & GADGET_FLAG_ENABLED)
+    {
+        if (gadget->properties.event_id != EVENT_NONE)
+        {
+            contraption_send_event(window->owner_thread, gadget->properties.event_id, window->id);
+        }
+        gadget->properties.state &= ~GADGET_STATE_ACTIVATED;
+        render_button(window, gadget);
+    }
 }
 
 
